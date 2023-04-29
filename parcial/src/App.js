c8952ac2dc0b2a0d225cc607d8292738c83fc6c2
Juan Pavón <98887772+JacobS1970@users.noreply.github.com>
2023-04-28 19:47:10 -0500

    eso

--- parcial/src/App.js
+++ parcial/src/App.js
@@ -1,10 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import PostList from './PostList';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data));
+  }, []);
+
   return (
-    <div className="App">
-      <PostList />
+    <div className="container">
+      <h1>Lista de posts</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Contenido</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id}>
+              <td>{post.userId}</td>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>{post.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
